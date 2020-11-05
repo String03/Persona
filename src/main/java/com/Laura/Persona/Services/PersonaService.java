7a1cdb8e12966e01d09046e503b41367d81841a5
analauramoyano <40239185+analauramoyano@users.noreply.github.com>
2020-11-05 11:38:21 -0300

    cambios db

--- src/main/java/com/Laura/Persona/Services/PersonaService.java
+++ src/main/java/com/Laura/Persona/Services/PersonaService.java
@@ -2,6 +2,11 @@ package com.Laura.Persona.Services;
 
 import com.Laura.Persona.EE.Persona;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "*")
+@RepositoryRestResource(collectionResourceRel = "personas", path = "persona")
 
 public interface PersonaService extends CrudRepository<Persona,Integer> {
 }
